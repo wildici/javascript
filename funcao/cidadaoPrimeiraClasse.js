afe08d457200cb59658844c137bdeeb8e2ec9e49
wildici <wildici@hotmail.com>
2019-09-20 21:46:00 -0300

    Update cidadaoPrimeiraClasse.js
    Add files folder function

--- funcao/cidadaoPrimeiraClasse.js
+++ funcao/cidadaoPrimeiraClasse.js
@@ -1,1 +1,32 @@
-ScriptProcessorNode
+// Forma literal
+function nome () {}
+
+//Armazenando em uma variável
+let f = function () {}
+
+//Armazenando em um array
+let array = [function () {}]
+
+//Armazenar em um atributo de objeto
+let obj = {}
+obj.falar = function () {return 'opa'}
+
+console.log(obj.falar())
+
+//Passar função como param
+function executar(run) {
+	run()
+}
+
+run(function () {
+	console.log('Executando...')
+})
+
+//Função pode retornar/conter uma função
+function soma(a, b) {
+	return function (c) {
+		console.log(a + b + c)
+	}
+}
+
+soma(2,3)(2)
